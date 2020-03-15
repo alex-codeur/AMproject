de6856d5003b22864dce70721bfb55b376008c02
Alex Esaki <apachecordovax@gmail.com>
2020-03-15 12:17:29 +0100

    Event Bus

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -5,8 +5,11 @@
             <div v-if="!editing" @dblclick="editTodo" class="todo-item-label" :class="{ completed : completed }">{{ title }}</div>
             <input v-else type="text" class="todo-item-edit" v-model="title" @blur="doneEdit" @keyup.enter="doneEdit" @keyup.esc="cancelEdit" v-focus>
         </div>
-        <div class="remove-item" @click="removeTodo(index)">
-            &times;
+        <div>
+            <button @click="pluralize">Plural</button>
+            <span class="remove-item" @click="removeTodo(index)">
+                &times;
+            </span>
         </div>
     </div>
 </template>
@@ -44,6 +47,12 @@ export default {
             }
         }
     },
+    created() {
+        eventBus.$on('pluralize', this.handlePluralize)
+    },
+    beforeDestroy() {
+        eventBus.$off('pluralize', this.handlePluralize)
+    },
     watch: {
         checkAll() {
             this.completed = this.checkAll ? true : this.todo.completed
@@ -51,7 +60,7 @@ export default {
     },
     methods: {
         removeTodo(index) {
-            this.$emit('removedTodo', index)
+            eventBus.$emit('removedTodo', index)
         },
         editTodo() {
             this.beforeEditCache = this.title
@@ -62,7 +71,7 @@ export default {
                 this.title = this.beforeEditCache
             }
             this.editing = false
-            this.$emit('finishedEdit', {
+            eventBus.$emit('finishedEdit', {
                 'index': this.index,
                 'todo': {
                     'id': this.id,
@@ -75,6 +84,21 @@ export default {
         cancelEdit() {
             this.title = this.beforeEditCache
             this.editing = false
+        },
+        pluralize() {
+            eventBus.$emit('pluralize')
+        },
+        handlePluralize() {
+            this.title = this.title + 's'
+            eventBus.$emit('finishedEdit', {
+                'index': this.index,
+                'todo': {
+                    'id': this.id,
+                    'title': this.title,
+                    'completed': this.completed,
+                    'editing': this.editing,
+                }
+            })
         }
     }
 }
